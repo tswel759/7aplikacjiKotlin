c24c69d2db34f3a5c92b7283a8f8791d5eaf527c
pc <szymon.radomski@cyfroweszkoly.pl>
2025-01-09 15:53:37 +0100

    dodanie cytatow do singletonu Quotes

---
+++ app/src/main/java/zad2Generator.kt
@@ -0,0 +1,14 @@
+object Quotes {
+    private val quotes = mutableListOf(
+        "Kto pod kim dolki kopie, ten szybciej awansuje.",
+        "Bogatym staje sie ten, kto okrada panstwo szybciej niz ono jego.",
+        "baba z wozu, koniom lzej.",
+        "co dwie glowy, to nie jedna."
+    )
+
+
+
+fun main() {
+
+}
+}
