7dfdffb7fa029f48eb14e643ccb1fd8a9b03b270
pc <szymon.radomski@cyfroweszkoly.pl>
2025-01-09 15:34:19 +0100

    stworzenie klasy Dice

--- app/src/main/java/zad1Roll.kt
+++ app/src/main/java/zad1Roll.kt
@@ -1,12 +1,18 @@
 interface RollingTool {
     fun roll(): String
 }
+
 class Coin : RollingTool {
     override fun roll(): String {
         return if ((1..2).random() == 1) "Heads" else "Tails"
     }
 }
 
+class Dice : RollingTool {
+    override fun roll(): String {
+        return (1..6).random().toString()
+    }
+}
 
 fun main() {
 }
