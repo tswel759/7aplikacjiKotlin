54e2fb38bf014a4018c8c6954f9097a33622b270
pc <szymon.radomski@cyfroweszkoly.pl>
2025-01-09 16:02:17 +0100

    ukonczenie zadania2

--- app/src/main/java/zad2Generator.kt
+++ app/src/main/java/zad2Generator.kt
@@ -13,8 +13,16 @@ object Quotes {
         quotes.add(quote)
     }
 
-
+}
     fun main() {
+        println("Losowy cytat: ${Quotes.getRandomQuote()}")
 
-}
-}
+        println("Podaj nowy cytat:")
+        val newQuote = readLine() ?: ""
+        if (newQuote.isNotEmpty()) {
+            Quotes.addQuote(newQuote)
+            println("Nowy cytat dodany!")
+        }
+
+        println("Nowy losowy cytat: ${Quotes.getRandomQuote()}")
+    }
