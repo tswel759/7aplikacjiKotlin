e3025f5e0cf20df726f3e97892aec39143ecb44e
pc <szymon.radomski@cyfroweszkoly.pl>
2025-01-09 15:33:07 +0100

    stworzenie klasy coin

--- app/src/main/java/zad1Roll.kt
+++ app/src/main/java/zad1Roll.kt
@@ -1,7 +1,11 @@
 interface RollingTool {
     fun roll(): String
 }
-
+class Coin : RollingTool {
+    override fun roll(): String {
+        return if ((1..2).random() == 1) "Heads" else "Tails"
+    }
+}
 
 
 fun main() {
